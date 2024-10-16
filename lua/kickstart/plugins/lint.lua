fb2f73c3ee67870c97ed3f23fc2d2043f17a6fbd
Rui Fonseca <804752+ruifonseca@users.noreply.github.com>
2024-10-16 19:52:08 +0100

    python, ruby, notes on markdown-lint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,10 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
+        -- $ npm install -g markdownlint-cli
         markdown = { 'markdownlint' },
+        python = { 'flake8' },
+        ruby = { 'rubocop' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
