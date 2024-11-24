9f1439e3a0e4104b8b76b2c5668e863ce7af8457
Rui Fonseca <804752+ruifonseca@users.noreply.github.com>
2024-11-24 11:13:30 +0000

    move back to pyright; use prettier

--- init.lua
+++ init.lua
@@ -631,29 +631,8 @@ require('lazy').setup({
         emmet_ls = {},
         clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        pylsp = {
-          settings = {
-            pylsp = {
-              plugins = {
-                -- formatter options
-                black = { enabled = true },
-                autopep8 = { enabled = false },
-                yapf = { enabled = false },
-                -- linter options
-                pylint = { enabled = true, executable = 'pylint' },
-                pyflakes = { enabled = false },
-                pycodestyle = { enabled = false },
-                -- type checker
-                pylsp_mypy = { enabled = true },
-                -- auto-completion options
-                jedi_completion = { fuzzy = true },
-                -- import sorting
-                pyls_isort = { enabled = true },
-              },
-            },
-          },
-        },
+        pyright = {},
+        -- pylsp = {},
         rust_analyzer = {},
         ruby_lsp = {},
         rubocop = {},
@@ -750,8 +729,9 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
-        css = { 'css', 'prettierd', 'prettier', stop_after_first = true },
-        scss = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettier' },
+        scss = { 'prettier' },
+        html = { 'prettier' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
@@ -880,15 +860,17 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'xero/miasma.nvim',
+    -- 'xero/miasma.nvim',
     -- 'nanotech/jellybeans.vim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'miasma'
+      -- vim.cmd.colorscheme 'miasma'
       -- vim.cmd.colorscheme 'jellybeans'
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
